98980bb406312117172a4e6be1b3e2960f3cf934
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-02 15:47:54 +0100

    :recycle: Code refactor

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ from repository.inventory import Inventario
 app = Flask(__name__)
 
 # Initial page #
-
+rutas = [["/nombres", "Devuelve los nombres de los items en el inventario"], 
+["/inventario", "Devuelve los items del inventario con su calidad y sell_in"], 
+["/quality/name", "Devuelve la calidad del objeto indicado"], 
+["/sell_in/name", "Devuelve el sell_in del objeto indicado"]]
 
 @app.route('/')
 def index():
     welcome = "<h1>Welcome to Olivanders</h1><br>"
     welcome += "<h3>Rutas habilitadas</h3>"
-    welcome += "<p>/nombres -> Devuelve los nombres de los items en el inventario</p>"
-    welcome += "<p>/inventario -> Devuelve los items del inventario con su calidad y sell_in</p>"
-    welcome += "<p>/quality/<name> -> Devuelve la calidad del objeto indicado</p>"
-    welcome += "<p>/sell_in/<name> -> Devuelve el sell_in del objeto indicado</p>"
+    
+    for entries in rutas:
+        welcome += "<p>" + entries[0] + " -> " + entries[1] + "</p>"
 
     return welcome
 
