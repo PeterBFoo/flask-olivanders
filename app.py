2f24cc260ebd22324fb046f193924a64715a2d71
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-08 16:50:39 +0100

    :sparkles: NEW routes added

--- app.py
+++ app.py
@@ -1,43 +1,56 @@
 from flask import Flask
-from repository.inventory import Inventario
+from controller.insert import Insert
+from controller.inventario import Inventario
+from controller.welcome import Welcome
+from controller.item import Item
+from controller.quality import Quality
+from controller.sell_in import SellIn
+from controller.delete import Delete
 
 app = Flask(__name__)
 
 # Initial page #
-rutas = [["/nombres", "Devuelve los nombres de los items en el inventario"], 
-["/inventario", "Devuelve los items del inventario con su calidad y sell_in"], 
-["/quality/name", "Devuelve la calidad del objeto indicado"], 
-["/sell_in/name", "Devuelve el sell_in del objeto indicado"]]
+
 
 @app.route('/')
 def index():
-    welcome = "<h1>Welcome to Olivanders</h1><br>"
-    welcome += "<h3>Rutas habilitadas</h3>"
-    
-    for entries in rutas:
-        welcome += "<p>" + entries[0] + " -> " + entries[1] + "</p>"
-
-    return welcome
+    return Welcome.initial_page()
 
 
 # View functions #
 
 
-@app.route('/nombres')
-def inventario():
-    return Inventario.getInventoryNames()
-
-
 @app.route('/inventario')
 def inventariosq():
-    return Inventario.getInventarioSQ()
+    return Inventario.getInventario()
 
 
-@app.route('/quality/<name>')
-def getQuality(name):
-    return Inventario.getQuality(name)
+@app.route('/quality/<num>')
+def getQuality(num):
+    return Quality.getItemQuality(num)
 
 
-@app.route('/sell_in/<name>')
-def getSellIn(name):
-    return Inventario.getSellIn(name)
+@app.route('/sell-in/<num>')
+def getSellIn(num):
+    return SellIn.getItemSellIn(num)
+
+
+@app.route('/item/<name>')
+def getItem(name):
+    return Item.getItem(name)
+
+
+@app.route('/insert/<item>/<quality>/<sell_in>/<clase>')
+def insertDocument(item, quality, sell_in, clase):
+    return Insert.insertarDocumento(item, quality, sell_in, clase)
+
+# @app.route('/update/<item>/<quality>/<sell-in>')
+
+
+# @app.route('/delete/<item>')
+# def deleteItem(item):
+#    return Delete.deleteDocument(item)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
