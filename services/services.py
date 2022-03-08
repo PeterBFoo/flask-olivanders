1c6fd3a9a94a5076d0af8ba9925f481a77623597
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-08 16:53:41 +0100

    :package::sparkles: New package and module Services

---
+++ services/services.py
@@ -0,0 +1,86 @@
+from repository.db import DB
+from flask import abort
+
+
+class Services:
+
+    @staticmethod
+    def getInventarioSQ():
+        db = DB.getQuery({})
+
+        inventario = {}
+        for entrance in db:
+            inventario[entrance["item"]] = {}
+            inventario[entrance["item"]]["quality"] = entrance["quality"]
+            inventario[entrance["item"]]["sell_in"] = entrance["sell_in"]
+
+        return inventario
+
+    @staticmethod
+    def getQuality(num):
+        try:
+            db = DB.getItemQuality(num)
+            query = {}
+
+            if (bool(db)):
+                for item in db:
+                    query[item["item"]] = {}
+                    query[item["item"]]["quality"] = item["quality"]
+                    query[item["item"]]["sell_in"] = item["sell_in"]
+
+                return query
+
+            else:
+                return "No se ha encontrado ningún objeto con la calidad " + num
+
+        except:
+            return "Algo ha salido mal", 500
+
+    @staticmethod
+    def getSellIn(num):
+        try:
+            db = DB.getItemSellIn(num)
+            query = {}
+
+            for item in db:
+                query[item["item"]] = {}
+                query[item["item"]]["quality"] = item["quality"]
+                query[item["item"]]["sell_in"] = item["sell_in"]
+
+            return query
+
+        except:
+            return "Algo ha salido mal", 500
+
+    @staticmethod
+    def getItem(name):
+        try:
+            db = DB.getItem(name)
+            query = {}
+
+            for item in db:
+                query[item["item"]] = {}
+                query[item["item"]]["quality"] = item["quality"]
+                query[item["item"]]["sell_in"] = item["sell_in"]
+
+            return query
+
+        except:
+            return "Algo ha salido mal", 500
+
+    @staticmethod
+    def insertItem(item, quality, sell_in, clase):
+        request = {"item": item, "quality": int(quality),
+                   "sell_in": int(sell_in), "_class": clase}
+        try:
+            DB.insertDocument(request)
+
+        except:
+            return "No se ha insertado el documento en la base de datos"
+
+        cursor = DB.getItem(request["item"])
+        for item in cursor:
+            if item["item"] == request["item"]:
+                return "Se ha insertado correctamente el documento en la base de datos -> \t" + item["item"] + ", \t" + str(item["quality"]) + ", \t" + str(item["sell_in"])
+            else:
+                return "Se ha insertado el documento en la base de datos, pero no se puede mostrar"
