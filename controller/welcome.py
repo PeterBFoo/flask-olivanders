c5492b0a782cd50bc2a2e554212d18f2013d1044
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-08 16:12:58 +0100

    :sparkles: New routes to show

--- controller/welcome.py
+++ controller/welcome.py
@@ -1,8 +1,19 @@
-from app import app
-
 
 class Welcome:
 
-    @app.route('/user/<name>')
-    def index(name):
-        return "<h1>Welcome to Olivanders, {}!</h1>".format(name)
+    def initial_page():
+        rutas = [
+            ["/inventario", "Devuelve los items del inventario con su calidad y sell_in"],
+            ["/quality/num", "Devuelve los objetos que tienen la calidad indicada"],
+            ["/sell_in/num", "Devuelve los objetos que tienen el sell_in indicado"],
+            ["/item/name", "Devuelve el item junto a su quality y sell_in"],
+            ["/insert/item/quality/sell_in/clase",
+                "Inserta un documento en la base de datos y seguidamente lo busca"]]
+
+        welcome = "<h1>Welcome to Olivanders</h1><br>"
+        welcome += "<h3>Rutas habilitadas</h3>"
+
+        for entries in rutas:
+            welcome += "<p>" + entries[0] + " -> " + entries[1] + "</p>"
+
+        return welcome
