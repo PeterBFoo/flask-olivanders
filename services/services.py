5edaefa9a66f7b2396a86fa1b81b823200e436f3
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-13 20:05:08 +0100

    :sparkles: Added service updateQuality

--- services/services.py
+++ services/services.py
@@ -1,4 +1,6 @@
 from repository.db import DB
+from repository.db_test import conectar_bd_test, getInventory
+from domain.types import *
 from flask import abort
 
 
@@ -78,8 +80,8 @@ class Services:
         except:
             return "No se ha insertado el documento en la base de datos"
 
-        cursor = DB.getItem(request["item"])
-        for item in cursor:
+        collection = DB.getItem(request["item"])
+        for item in collection:
             if item["item"] == request["item"]:
                 return "Se ha insertado correctamente el documento en la base de datos"
             else:
@@ -88,11 +90,12 @@ class Services:
     @staticmethod
     def deleteDocument(item):
         try:
-            delete = DB.deleteDocument(item)
-            if (Services.getItem(item) == {}):
-                return "No existe el item indicado"
-            else:
+            if (Services.getItem(item) != {}):
+                delete = DB.deleteDocument(item)
                 return "El item " + item + " ha sido eliminado"
+
+            else:
+                return "No se ha encontrado el item " + item + " en la base de datos"                
 
         except:
             return "Algo ha salido mal"
@@ -100,12 +103,90 @@ class Services:
     @staticmethod
     def updateDocument(item, quality, sell_in):
         try:
-            objeto = Services.getItem(item)
-            if (objeto != {}):
+            if (Services.getItem(item) != {}):
                 DB.updateDocument(item, quality, sell_in)
-                return "El item " + item + " ha sido actualizado"
+                return Services.getItem(item)
             else:
                 return "El item " + item + " no existe"
 
         except:
             return "Algo ha salido mal"
+
+    @staticmethod
+    def updateQuality(test):
+        ## Conecta con la base de datos para obtener todo el inventario ##
+        if test == 1:
+            connection = conectar_bd_test()
+            db = connection.find({})
+        elif test == 0:
+            db = DB.getQuery({})
+
+        inventario = {}
+
+        for entrance in db:
+            inventario[entrance["item"]] = {}
+            inventario[entrance["item"]]["quality"] = entrance["quality"]
+            inventario[entrance["item"]]["sell_in"] = entrance["sell_in"]
+            inventario[entrance["item"]]["_class"] = entrance["_class"]
+
+        ## TIPOS = ["NormalItem", "ConjuredItem", "AgedBrie", "Sulfuras", "BackstagePass"] ##
+        updatedInventario = {}
+        for item in inventario:
+            if inventario[item]["_class"] == "NormalItem":
+                updateItem = NormalItem(
+                    inventario[item], inventario[item]["sell_in"], inventario[item]["quality"])
+                updateItem.update_quality()
+
+                updatedInventario[item] = {}
+                updatedInventario[item]["quality"] = updateItem.quality
+                updatedInventario[item]["sell_in"] = updateItem.sell_in
+
+            elif inventario[item]["_class"] == "ConjuredItem":
+                updateItem = ConjuredItem(
+                    inventario[item], inventario[item]["sell_in"], inventario[item]["quality"])
+                updateItem.update_quality()
+
+                updatedInventario[item] = {}
+                updatedInventario[item]["quality"] = updateItem.quality
+                updatedInventario[item]["sell_in"] = updateItem.sell_in
+
+            elif inventario[item]["_class"] == "AgedBrie":
+                updateItem = AgedBrie(
+                    inventario[item], inventario[item]["sell_in"], inventario[item]["quality"])
+                updateItem.update_quality()
+
+                updatedInventario[item] = {}
+                updatedInventario[item]["quality"] = updateItem.quality
+                updatedInventario[item]["sell_in"] = updateItem.sell_in
+
+            elif inventario[item]["_class"] == "Sulfuras":
+                updateItem = Sulfuras(
+                    inventario[item], inventario[item]["sell_in"], inventario[item]["quality"])
+                updateItem.update_quality()
+
+                updatedInventario[item] = {}
+                updatedInventario[item]["quality"] = updateItem.quality
+                updatedInventario[item]["sell_in"] = updateItem.sell_in
+
+            elif inventario[item]["_class"] == "BackstagePass":
+                updateItem = BackstagePass(
+                    inventario[item], inventario[item]["sell_in"], inventario[item]["quality"])
+                updateItem.update_quality()
+
+                updatedInventario[item] = {}
+                updatedInventario[item]["quality"] = updateItem.quality
+                updatedInventario[item]["sell_in"] = updateItem.sell_in
+
+        if test == 1:
+            for item in updatedInventario:
+                query = {"item": item}
+                newvalues = {"$set": {
+                    "quality": updatedInventario[item]["quality"], "sell_in": updatedInventario[item]["sell_in"]}}
+                connection.update_one(query, newvalues)
+            return getInventory()
+
+        else:
+            for item in updatedInventario:
+                Services.updateDocument(
+                    item, updatedInventario[item]["quality"], updatedInventario[item]["sell_in"])
+            return Services.getInventarioSQ()
