53ce9fe5b27287d098ad5ac61f8b95f454f3ae5b
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-03-14 00:48:11 +0100

    :rewind: Reverted changes

--- app.py
+++ app.py
@@ -55,8 +55,4 @@ def updateQuality():
     return UpdateQuality.updateQuality(0)
 
 if __name__ == "__main__":
-    # query = inventariosq()
-    # if query == {}:
-    #    DB.init_db()
-    url = "http://127.0.0.1:5000"
-    app.run(use_reloader=False, debug=True, port=5000)
+    app.run()
